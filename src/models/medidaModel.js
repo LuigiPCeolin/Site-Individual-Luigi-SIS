e2d81ff6231d7d3153e97ca4a099848ad3fd7ae8
LuigiPCeolin <luigi.ceolin@sptech.school>
2022-06-07 19:23:52 -0300

    Terminei sera

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -7,18 +7,6 @@ function buscarUltimasMedidas(idUsuario) {
     return database.executar(instrucaoSql);
 }
 
-// function buscarMedidasEmTempoReal(idAquario) {
-//     instrucaoSql = `select 
-//                         temperatura, 
-//                         umidade, DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-//                         fk_aquario 
-//                         from medida where fk_aquario = ${idAquario} 
-//                     order by id desc limit 1`;
-
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
-
 
 module.exports = {
     buscarUltimasMedidas
